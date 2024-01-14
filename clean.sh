1bd8016247cebb522475d6dbd99542965eb48a8f
kawaiiandgothic <67576969+kawaiiandgothic@users.noreply.github.com>
2024-01-14 08:01:21 -0600

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-sudo apt install gnome-core gnome-shell-extension-manager gnome-tweaks && sudo apt purge task-gnome-desktop
+
+sudo apt install qt5-style-kvantum gnome-software-plugin-flatpak flatpak gnome-core gnome-shell-extension-manager gnome-tweaks && sudo apt purge task-gnome-desktop
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.kde.KStyle.Kvantum
+kvantummanager -set KvGnomeDark
